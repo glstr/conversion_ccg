036f7b7a307b84dff0c824094b1c66f9839f5348
glstr <glstrsc@163.com>
2019-03-05 00:46:31 +0800

    add basic example

--- src/cgal_operator.cpp
+++ src/cgal_operator.cpp
@@ -1,21 +1,80 @@
 
 #include "cgal_operator.h"
 
+#include <iostream>
+
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/draw_polyhedron.h>
+
 namespace snow {
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_2 Point_2;
+typedef Kernel::Segment_2 Segment_2;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+typedef Polyhedron::Halfedge_handle Halfedge_handle;
+
+using namespace std;
+
 void CGALOperator::process() {
     int op_code = get_opcode();
     switch (op_code) {
-        case Default:
-            
+        case PolyhedronOpe:
+            polyhedron_ope();
+            break;        
+        case HelloWorld:
+            hello_world();
+            break;
+        case ShowExample:
+            show_example();
+            break;
+        default:
+            usage();
     } 
+    return;
 }
 
 CGALOperator::OpCode CGALOperator::get_opcode() {
     if (_flag == "default") {
-        return Default;
+        return PolyhedronOpe;
+    } else if (_flag == "helloworld") {
+        return HelloWorld;
+    } else if (_flag == "polyhedron_ope") {
+        return PolyhedronOpe;
+    } else if (_flag == "show_example") {
+        return ShowExample;
     }
     return UnKnown;
 }
 
+void CGALOperator::polyhedron_ope() {
+    
 }
+
+void CGALOperator::hello_world() {
+    Point_2 p(1, 1), q(10, 10);            
+    std::cout << "p=" << p << std::endl; 
+    std::cout << "q = " << q.x() << " " << q.y() << std::endl;
+}
+
+void CGALOperator::show_example() {
+    //poly simple
+    poly_simple();    
+}
+
+void CGALOperator::poly_simple() {
+    Polyhedron p;
+    Halfedge_handle h = p.make_tetrahedron();    
+    if (p.is_tetrahedron(h)) {
+        cout << "0" << endl;
+        //CGAL::draw(p);
+        return;
+    }
+}
+
+void CGALOperator::usage() {
+    cout << "hello world" << endl;
+}
+
+} //end namespace snow;
 
--- src/cgal_operator.h
+++ src/cgal_operator.h
@@ -8,22 +8,37 @@ class CGALOperator {
 public:
     enum OpCode {
         UnKnown = 0,
-        Default = 1,
+        PolyhedronOpe = 1,
+        HelloWorld = 2,
+        ShowExample,
     };
 
-    CGALOperator(const std::string& file_in, const std::string& flag) : 
+    CGALOperator(const std::string& file_in, const std::string& file_out, 
+            const std::string& flag) : 
         _file_in(file_in),
         _flag(flag) {}
     virtual ~CGALOperator(void) {} 
 
     void process();
+
 private:
     OpCode get_opcode();
+    void polyhedron_ope();
+    void hello_world();
+    void usage();
+
+    //example 
+    void show_example();
+
+    //polyhedra example
+    void poly_simple();
+    
 
     std::string _file_in;
+    std::string _file_out;
     std::string _flag;
 };
 
-}
+} //end namespace snow;
 
 
